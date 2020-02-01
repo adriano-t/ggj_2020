6a8a422c7fc3f4196f66256206524b30ad0fe049
baloonSpot <serygr@gmail.com>
2020-02-01 19:44:32 +0100

    pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void BackToGame()
+    {
+        SceneManager.LoadScene("Map");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
