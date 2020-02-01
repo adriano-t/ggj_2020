83a52f354604debf50e7b1dae0bcfb8d27237a5e
Alex Salemi <alex_s96@hotmail.it>
2020-02-01 12:47:44 +0100

    Aggiunto Cell.cs per le celle. Aggiunto CellEvent.cs per gli eventi sulle celle.

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -2,19 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Cell : MonoBehaviour
 {
+    private float temperature;
+    private float waterLevel;
+
+    private CellEvent currentEvent;
+
+    public float Temperature
+    {
+        get { return this.temperature; }
+        set { this.temperature = Mathf.Min(0, Mathf.Max(this.temperature + value, 100)); }
+    }
     
+    public float WaterLevel
+    {
+        get { return this.waterLevel; }
+        set { this.waterLevel = Mathf.Min(0, Mathf.Max(this.waterLevel + value, 100)); }
+    }
     
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (this.currentEvent != null)
+        {
+            this.currentEvent.Update();
+
+            if (this.currentEvent.isOver())
+            {
+                this.currentEvent == null;
+            }
+        }
     }
+
+    public void setCellEvent(CellEvent cellEvent)
+    {
+        this.currentEvent = cellEvent;
+        this.currentEvent.Start();
+    }
+
 }
---
+++ Assets/Scripts/CellEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Evento che accade in una determinata cella (incendio, ecc)
+public abstract class CellEvent
+{
+    private Cell cell;
+    
+    public abstract void Start();
+
+    public abstract void Update();
+
+    public abstract bool isOver();
+}
