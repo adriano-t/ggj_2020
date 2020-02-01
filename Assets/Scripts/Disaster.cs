81b50e8eaf77dba4afd3e83ee2a7556bc7e8cdf3
Adriano Tumminelli <adriano.tummi@hotmail.it>
2020-02-01 18:26:14 +0100

    ok

--- Assets/Scripts/Disaster.cs
+++ Assets/Scripts/Disaster.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Disaster : MonoBehaviour
 {
-    // Start is called before the first frame update
+     
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+     
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Weapon : MonoBehaviour
-{
-    public ParticleSystem particles;
-    public GameObject[] weapons;
+{ 
+    public struct StructWeapon
+    {
+        public ParticleSystem particles;
+        public GameObject obj;
+    }
+    public StructWeapon[] weapons;
     public int selectedWeapon = 0;
     public float weaponRange = 2.0f;
     void Start()
@@ -16,33 +21,31 @@ public class Weapon : MonoBehaviour
     private void RefreshWeapon ()
     {
         //deactivate all the weapons except the selected
-        for (int i = 0; i < transform.childCount; i++)
+        for (int i = 0; i < weapons.Length; i++)
             if (i != selectedWeapon)
-                transform.GetChild(i).gameObject.SetActive(false);
+                weapons[i].obj.SetActive(false);
     }
     
     public void GetNextWeapon()
-    {
-        if (selectedWeapon + 1 < weapons.Length)
-        {
-            selectedWeapon += 1;
-            RefreshWeapon();
-        }
+    { 
+        selectedWeapon = (selectedWeapon + 1) % weapons.Length;
+        RefreshWeapon();
     }
 
     public void GetPreviousWeapon()
     {
-        if (selectedWeapon - 1 >= 0)
-        {
-            selectedWeapon -= 1;
-            RefreshWeapon();
-        }
+        selectedWeapon--;
+        if(selectedWeapon < 0)
+            selectedWeapon = weapons.Length - 1;
+
+        RefreshWeapon();
     }
 
     public void Shoot ()
     {
-        particles.Play();
-        Physics.OverlapCapsule(transform.position, transform.position + transform.forward * weaponRange, 0.5f);
+        weapons[selectedWeapon].particles.Play();
+        var colliders = Physics.OverlapCapsule(transform.position, transform.position + transform.forward * weaponRange, 0.5f);
+
         Debug.DrawLine(transform.position, transform.position + transform.forward * weaponRange, Color.red, 10.0f);
     }
 }
