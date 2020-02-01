c838e35a5ea08ef8c88a6b15f9c0de4e39991832
Alex Salemi <alex_s96@hotmail.it>
2020-02-01 12:08:13 +0100

    Reset prova fallimentare dell hexasfera.

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -6,33 +6,9 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-	[SerializeField]
-	private GameObject hexagon = null;
-	
 	void Start()
 	{
-		double posX = 0;
-		double posY = 0;
-		double posZ = 0;
-
-		double dist = GetRadius();
-
-		for (double i = 0; i < 2*Math.PI; i += Math.PI/5)
-		{
-			//for (double j = 0; j < 2*Math.PI; j += Math.PI/5)
-			{
-				posX = this.transform.position.x + Math.Cos(i) * dist;
-				posY = this.transform.position.z + Math.Sin(i) * dist;
-				posY = 
-
-				Debug.Log(posX + ", " + posY);
-				
-				GameObject obj = Instantiate(hexagon, null);
-				obj.transform.position = new Vector3((float) posX, (float) posY, 0);
-				// obj.transform.rotation = new Q
-				transform.rotation = Quaternion.Euler(MAIN.GetDir(GetCenter(), obj.transform.position));
-			}
-		}
+		
 	}
 
 	// Update is called once per frame
