4d0721314eea333719da2ed0ea9c17f4ef3f39d6
baloonSpot <serygr@gmail.com>
2020-02-01 16:36:23 +0100

    Main menu

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    public void SetMusicVolume(float volume)
+    {
+        
+    }
+
+    public void SetSoundVolume(float volume)
+    {
+
+    }
+}
