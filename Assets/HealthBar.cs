19e56cf4c435d994b6931e6b20919f70fdd5a863
Adriano Tumminelli <adriano.tummi@hotmail.it>
2020-02-02 10:44:03 +0100

    aaaq

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image[] hudCells;
+
+    public void SetValue (float atmosphereValue)
+    { 
+        int val = Mathf.RoundToInt(hudCells.Length * Mathf.Clamp01(atmosphereValue));
+        for (int i = 0; i < hudCells.Length; i++)
+        {
+            hudCells[i].transform.GetChild(0).gameObject.SetActive(i < val);
+        }
+
+
+    }
+}
