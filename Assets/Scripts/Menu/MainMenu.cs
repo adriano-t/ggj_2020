07a912cfcd218340db29cda6eaaf25f36011c247
Tizzio <adriano.tummi@hotmail.it>
2020-02-04 02:24:32 +0100

    main menu reworked

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Slider vol, FX;
+    public Toggle toggleSticks;
+     
+    private void Start ()
+    {
+        vol.SetValueWithoutNotify(MAIN.opVolumeMusicMult);
+        FX.SetValueWithoutNotify(MAIN.opVolumeFXmult);
+    }
+
+    #region Main
     public void PlayGame()
     {
         SceneManager.LoadScene("Map");
@@ -14,4 +26,34 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+    #endregion
+
+    #region Options
+
+    public void SetMusicVolume ()
+    {
+        MAIN.opVolumeMusicMult = vol.value;
+    }
+
+    public void SetSoundVolume ()
+    {
+        MAIN.opVolumeFXmult = FX.value;
+    }
+     
+    public void ToggleSticks()
+    {
+        //todo
+        bool dualsticks = toggleSticks.isOn;
+    }
+
+    public void Save ()
+    {
+        string[] data = {
+            MAIN.opVolumeMusicMult.ToString(),
+            MAIN.opVolumeFXmult.ToString()
+        };
+
+        File.WriteAllLines(Application.persistentDataPath + "/settings.txt", data);
+    }
+    #endregion
 }
