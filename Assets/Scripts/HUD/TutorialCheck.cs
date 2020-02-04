ceac81f34e7cf330882e8b924022cf06c8d04ea6
cla8194 <cp080194@hotmail.it>
2020-02-04 14:17:04 +0100

    tutorial check

---
+++ Assets/Scripts/HUD/TutorialCheck.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialCheck : MonoBehaviour
+{
+    public GameObject tutKey, tutPad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool j = MAIN.ControllerConnected();
+
+        tutKey.SetActive(!j);
+        tutPad.SetActive(j);
+    }
+}
